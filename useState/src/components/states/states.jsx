021fb89aea22afd5db2b39cf7b74e0028a3bb9db
1siikaa <srishtidwivedi672@gmail.com>
2023-05-08 15:11:43 +0530

    done

---
+++ useState/src/components/states/states.jsx
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+
+export default State = () =>{
+//    const [newstate, setNewstate ] = useState({
+//     firstName: "",
+//     lastName:"",
+//     Contact: "",
+//     Email:""
+
+//    })
+//    const [count, setCount] = useState(0)
+//    const [message, setMessage] = useState('')
+    return(
+        <div>
+  <h1>get started </h1>
+        </div>
+    )
+}
