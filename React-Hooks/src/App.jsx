0a694b20ae595650f93c6040c74b28a025f2a64f
Dipesh-J <dipeshjoshi015@gmail.com>
2023-05-08 14:39:27 +0530

    created

---
+++ React-Hooks/src/App.jsx
@@ -0,0 +1,16 @@
+import  React from 'react'
+import './App.css'
+import LearningUseState from './components/stateComponent/useState'
+
+function App() {
+  return (
+    <>
+      <p>
+        Click on the Vite and React logos to learn more
+      </p>
+      <LearningUseState/>
+    </>
+  )
+}
+
+export default App
---
+++ React-Hooks/src/components/stateComponent/useState.jsx
@@ -0,0 +1,11 @@
+import { React } from "react";
+
+function LearningUseState() {
+  return (
+    <div>
+      <h1> USE-STATE HOOK</h1>
+    </div>
+  );
+}
+
+export default LearningUseState;
